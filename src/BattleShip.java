eb225d0516a7f54f89aad104c33afc92895e5f2d
MarioMaldonadoS <mariomaldonados98@gmail.com>
2018-03-24 14:39:18 -0700

    added image directory to project

--- src/BattleShip.java
+++ src/BattleShip.java
@@ -1,4 +1,16 @@
+import javax.swing.*;
+import java.awt.*;
+import java.io.*;
 
 public class BattleShip {
-
+    public static void main(String[] args){
+        JFrame f = new JFrame("Battleship");
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        f.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        f.setUndecorated(true);
+        
+        f.pack();
+        f.setVisible(true);
+    }
 }
